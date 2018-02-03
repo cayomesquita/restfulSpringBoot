faf443071b5ac660216f72879db9fd466ddc6711
Cayo <cayomesquita@gmail.com>
2018-02-03 01:05:18 -0200

    mapping product resource

--- src/main/java/org/cayo/handson/restful/JerseyConfig.java
+++ src/main/java/org/cayo/handson/restful/JerseyConfig.java
@@ -2,11 +2,12 @@ package org.cayo.handson.restful;
 
 import javax.ws.rs.ApplicationPath;
 
+import org.cayo.handson.restful.endpoints.HelloEndpoint;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ApplicationPath("/jersey")
+@ApplicationPath("/api")
 public class JerseyConfig extends ResourceConfig {
 
 	public JerseyConfig() {
